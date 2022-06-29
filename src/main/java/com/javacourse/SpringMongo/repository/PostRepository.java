a60eb07e044cf127220db589e2f5be6d00c31b3e
Letícia Dupont <leticia.dupont20@gmail.com>
2022-06-29 15:19:28 -0300

    @Query methods of spring - regex

--- src/main/java/com/javacourse/SpringMongo/repository/PostRepository.java
+++ src/main/java/com/javacourse/SpringMongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.javacourse.SpringMongo.repository;
 import com.javacourse.SpringMongo.domain.Post;
 import com.javacourse.SpringMongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +11,7 @@ import java.util.List;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{'title' : { $regex: ?0, $options: 'i' } }")
+    List<Post> findByTitle(String text);
 }
