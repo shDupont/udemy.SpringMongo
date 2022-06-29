5d4ac462559bdd3f74adfb5bfff34cbe5d5b8090
Letícia Dupont <leticia.dupont20@gmail.com>
2022-06-29 11:11:39 -0300

    Creating domains

---
+++ src/main/java/com/javacourse/SpringMongo/domain/Post.java
@@ -0,0 +1,66 @@
+package com.javacourse.SpringMongo.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class Post implements Serializable {
+    private String id;
+    private Date date;
+    private String title;
+    private String body;
+
+    public Post(){}
+
+    public Post(String id, Date date, String title, String body) {
+        this.id = id;
+        this.date = date;
+        this.title = title;
+        this.body = body;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return Objects.equals(getId(), post.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
