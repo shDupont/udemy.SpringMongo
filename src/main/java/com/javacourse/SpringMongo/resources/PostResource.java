fe385ff29b72b5da2ccf3226bbd2e52c0eb55534
Letícia Dupont <leticia.dupont20@gmail.com>
2022-06-29 14:47:47 -0300

    GET: post by id

---
+++ src/main/java/com/javacourse/SpringMongo/resources/PostResource.java
@@ -0,0 +1,24 @@
+package com.javacourse.SpringMongo.resources;
+
+import com.javacourse.SpringMongo.domain.Post;
+
+import com.javacourse.SpringMongo.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService postService;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Post> findById(@PathVariable String id){
+        Post obj = postService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+
+}
---
+++ src/main/java/com/javacourse/SpringMongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.javacourse.SpringMongo.services;
+
+
+import com.javacourse.SpringMongo.domain.Post;
+import com.javacourse.SpringMongo.repository.PostRepository;
+import com.javacourse.SpringMongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post findById(String id){
+        Post post = postRepository.findById(id).orElse(null);
+        if (post == null){
+            throw new ObjectNotFoundException("Object not found");
+        }
+        return post;
+    }
+}
